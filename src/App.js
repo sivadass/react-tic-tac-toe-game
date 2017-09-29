e185f009d503b8ff78bf3f414eb9ae75c72275e8
Sivadass N <nsivadass@gmail.com>
2017-09-29 21:34:59 +0530

    Game UI added.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      PLAYER_ONE_SYMBOL: "X",
+      PLAYER_TWO_SYMBOL: "O",
+      currentTurn: "X",
+      board: [
+        "", "", "", "", "", "", "", "", ""
+      ],
+      winner: null,
+    }
+  }
+
+  handleClick(index) {
+    if(this.state.board[index] === "" && !this.state.winner) {
+      this.state.board[index] = this.state.currentTurn
+      this.setState({
+        board: this.state.board,
+        currentTurn: this.state.currentTurn === this.state.PLAYER_ONE_SYMBOL ? this.state.PLAYER_TWO_SYMBOL : this.state.PLAYER_ONE_SYMBOL,
+        winner: this.checkForWinner(),
+      })
+    }
+  }
+
+  checkForWinner() {
+    var currentTurn = this.state.currentTurn
+    var symbols = this.state.board
+    var winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    return winningCombos.find(function(combo) {
+      if(symbols[combo[0]] !== "" && symbols[combo[1]] !== ""  && symbols[combo[2]] !== ""  && symbols[combo[0]] === symbols[combo[1]] && symbols[combo[1]] === symbols[combo[2]]) {
+        return currentTurn
+      } else {
+        return false
+      }
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app-container">
+        <h1>React Tic Tac Toe Game</h1>
+        <div className="board">
+        {this.state.board.map((cell, index) => {
+          return <div key={index} onClick={() => this.handleClick(index)} className="square">{cell}</div>;
+        })}
+        </div>
+        {this.state.winner ? <h2 className="winner">{`The winner is ${this.state.winner}`}</h2> : null}
       </div>
-    );
+    )
   }
 }
 
